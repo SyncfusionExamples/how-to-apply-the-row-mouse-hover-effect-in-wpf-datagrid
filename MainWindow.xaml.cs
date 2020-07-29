28d739d013aaa554e37f8b894250f640a221d7d0
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2020-07-29 20:01:38 +0530

    Updatedthe codebehind changes
    Updatedthe codebehind changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,24 +17,8 @@ namespace SfDataGridDemo
     {
         public MainWindow()
         {
-            InitializeComponent();
-            this.sfDataGrid.MouseMove += SfDataGrid_MouseMove;
-        }
-
-        private void SfDataGrid_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            //With the help of VisualContainer ,you can get the row and column index based on the mouse move pointer position
-            var visualcontainer = this.sfDataGrid.GetVisualContainer();
-            // Gets the exact position where the mouse pointer is moved 
-            var point = e.GetPosition(visualcontainer);
-            //Here you can get the row and column index based on the pointer position by using PointToCellRowColumnIndex() method
-            var rowColumnIndex = visualcontainer.PointToCellRowColumnIndex(point);
-            if (!rowColumnIndex.IsEmpty)
-            {
-                sfDataGrid.AllowRowHoverHighlighting = true;
-                sfDataGrid.RowHoverHighlightingBrush = Brushes.Yellow;
-            }
-        }
+            InitializeComponent();           
+        }       
     }  
 }
 
